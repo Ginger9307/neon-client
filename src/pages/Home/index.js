52e9a1a74bdc7bf653e45f19c4924c3b90406c11
“Ginger9307” <tvlezhneva@gmail.com>
2022-02-07 22:04:45 +0000

    add power button styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,13 +10,13 @@ const Home = () => {
         <h1 className='glow'>MENU</h1>
         <div className='container menu'>
         
-            <a href='/create' className='button btn-ng' id=''>NEW GAME {' >>'} </a> 
+            <a href='/create' className='btn-ng' id=''>NEW GAME {' >>'} </a> 
 
-            <a href='/join' className='button btn-jg'id=''>JOIN GAME {' >>'} </a> 
+            <a href='/join' className='btn-jg'id='' id=''>JOIN GAME {' >>'} </a> 
 
-            <a href='/leaderboard' className='button btn-lb' id=''>LEADERBOARD {' >>'} </a>
+            <a href='/leaderboard' className='btn-lb' id=''>LEADERBOARD {' >>'} </a>
 
-            <a href='/about' className='btn-a button' id=''>ABOUT {' >>'} </a>
+            <a href='/about' className='btn-a' id=''>ABOUT {' >>'} </a>
 
         </div>); 
     </div>);
