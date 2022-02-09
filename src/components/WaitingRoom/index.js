4ecdc7c45d7dc6fd4d64e065d5809d86d135b023
Hayden Kidd <hkidd@live.co.uk>
2022-02-09 16:21:56 +0000

    added a player list for admin waiting room

--- src/components/WaitingRoom/index.js
+++ src/components/WaitingRoom/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { useSelector } from 'react-redux';
 import { useSocket } from '../../contexts/SocketProvider';
@@ -13,6 +13,7 @@ const WaitingRoom = () => {
   const difficulty = useSelector(state => state.difficulty);
   const numQ = useSelector(state => state.numQ);
   const questions = useSelector(state => state.questions);
+  const [newPlayers, setNewPlayers] = useState([]);
 
   const handleNext = (() => {
     socket.emit('start-game', room, difficulty, numQ, questions);
@@ -25,14 +26,18 @@ const WaitingRoom = () => {
 
   useEffect(() => {
     socket.on('user-join', playerName => {
-      //write function to add names to list
-      console.log(playerName)
+      setNewPlayers(newPlayers => [...newPlayers, playerName])
     });
   }, [socket]);
+
+  const renderPlayers = () => newPlayers.map(newPlayer => <li>{newPlayer}</li>)
   
   return (
     <div>
         <h2> Waiting for others players to join the game </h2>
+        <ul>
+          {renderPlayers()}
+        </ul>
         <a href='/menu'className='btn-nav'>{'<< '}back </a> 
         <a onClick={handleNext} className='btn-nav'>start {' >>'} </a> 
 
