0e6fba25a2df0f0f9f75c65a7fef413e664d6d0f
Hayden Kidd <hkidd@live.co.uk>
2022-02-09 12:10:32 +0000

    started multiplayer functionality

--- src/components/WaitingRoomJoin/index.js
+++ src/components/WaitingRoomJoin/index.js
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useSocket } from '../../contexts/SocketProvider';
+import { loadQuestions, createGame } from '../../actions';
 
 
 const WaitingRoomJoin = () => {
 
   const history = useHistory(); 
   const socket = useSocket();
+  const dispatch = useDispatch();
   const room = useSelector(state => state.room);
 
+  useEffect(() => {
+    socket.on('init-game', (diff, qnum, quiz) => {
+      dispatch(loadQuestions(quiz));
+      history.push('/game');
+    });
+  }, [socket]);
   
   return (
     <div>
