fd1f4d3efde06d51e36b920240923613ca8d9d78
Hayden Kidd <hkidd@live.co.uk>
2022-02-09 10:34:45 +0000

    added socket.io

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { SocketProvider } from './contexts/SocketProvider';
 import store from './store';
 import App from './App';
 import './index.css';
@@ -9,11 +10,13 @@ import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <Router>
-              <App />
-            </Router>
-        </Provider>
+        <SocketProvider>
+            <Provider store={store}>
+                <Router>
+                <App />
+                </Router>
+            </Provider>
+        </SocketProvider>
     </React.StrictMode>,
 document.getElementById("root")
 );
